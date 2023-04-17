23df6ae1f6f8fec4a7c2de95a031639f2b6906ee
Digital-S <127454668+DigitalSerpant@users.noreply.github.com>
2023-04-17 10:26:58 -0500

    Update script.js

--- Sub/script.js
+++ Sub/script.js
@@ -9,6 +9,12 @@ else{
 function sus() {
   Cookies.set('Verifide', 'true', { expires: 1 })  
       };
+
 window.addEventListener("beforeunload", function(event) {
-  Cookies.set('Verifide', 'close', { expires: 1 })
+  Cookies.set('Verifide', 'close', { expires: 0.2})
+  this.close();
 });
+var myCookie = Cookies.get('Verifide');
+if(myCookie==='close'){
+  this.close();
+}
